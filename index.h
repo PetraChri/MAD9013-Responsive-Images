5933e3addf03bedfb3e8ebbf901fe8fb63c36974
PetraChri <chri0195@algonquinlive.com>
2015-10-26 17:59:26 -0400

    Create index.h

---
+++ index.h
@@ -0,0 +1,137 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Responsive Layout and Picture Element</title>
+    <meta name="handheldfriendly" content="true" />
+    <meta name="mobileoptimized" content="240" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,target-densitydpi=device-dpi" />
+    <link href="pictures.css" rel="stylesheet" type="text/css">
+</head>
+
+    
+<body>
+
+<div class="wrap">
+    <div class="banner"></div>
+    
+    <div class="wrapper">
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+      <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+      <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    <picture>
+       <source media="(max-width: 400px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 600px)" srcset="http://nicenicejpg.com/600/600">
+       <source media="(max-width: 800px)" srcset="http://nicenicejpg.com/400/400">
+       <source media="(max-width: 1020px)" srcset="http://nicenicejpg.com/512/512">
+       <source media="(min-width: 1024px)" srcset="http://nicenicejpg.com/340/340">
+       <source src="http://nicenicejpg.com/340/340">
+       <img src="http://placehold.it/340x340" alt="This picture loads on non-supporting browsers.">
+    </picture>
+    
+     
+    </div>
+</div>
+
+</body>
+
+    
+<footer class="footer">
+  <p>Copyright MySite 1954<p>
+</footer>
